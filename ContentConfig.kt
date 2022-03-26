435123e8e4a58441edc9de8670282a140fa40ec5
jeki21 <101493332+jeki21@users.noreply.github.com>
2022-03-26 19:11:14 +0700

    Add files via upload

---
+++ ContentConfig.kt
@@ -0,0 +1,19 @@
+package com.joksee.Blackaestheticwallpapers.config
+
+// username akun
+var USERNAME = "bngjeck992"
+
+// base name album (board)
+var ALBUM = listOf("cat")
+
+// JANGAN DIUBAH - URGENT !
+var JUMLAH_PHOTO_PER_ALBUM = 50
+
+//Boleh diubah, kelipatan 50
+var INIT_JUMLAH_PHOTO_PER_ALBUM = 50
+
+// fcm topic untuk push notitifcation
+const val APP_FCM_TOPIC = "joksee_Blackaestheticwallpapers_topic"
+
+const val SHOW_PRIVACY_POLICY = false // ubah ke false jika tidak ingin menggunakan privacy policy
+const val PRIVACY_POLICY_URL = "https://www.google.com"
